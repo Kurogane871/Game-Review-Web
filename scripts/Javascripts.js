9476f3a101929975b9076e507d555ceda63fa567
Kurogane871 <mkdreadsword@gmail.com>
2026-03-04 17:18:17 -0800

    Prof Jay is still testing yet again yet again ... I am done i guess not

--- scripts/Javascripts.js
+++ scripts/Javascripts.js
@@ -1,15 +1,15 @@
 fetch('./nav.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById("nav-component").innerHTML = data;
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("nav-component").innerHTML = data;
 
-    //creating variables to hold the element reference
-    // make a reference for the element
-    const hamburger = document.getElementById("hamburger");
-    const navList = document.getElementById("nav-list");
+        //creating variables to hold the element reference
+        // make a reference for the element
+        const hamburger = document.getElementById("hamburger");
+        const navList = document.getElementById("nav-list");
 
-    hamburger.addEventListener("click", () => {
-        navList.classList.toggle("active");
-    });
-})
-.catch(error => console.error("Nav didn't load", error));
+        hamburger.addEventListener("click", () => {
+            navList.classList.toggle("active");
+        });
+    })
+    .catch(error => console.error("Nav didn't load", error));
